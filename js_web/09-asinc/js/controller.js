5e6fcf08b8251e00361661953b7bf52eb15e68c0
alumno <alumno@formacion.local>
2019-05-20 09:39:08 +0200

    corregido 20/05

--- js_web/09-asinc/js/controller.js
+++ js_web/09-asinc/js/controller.js
@@ -1,58 +1,58 @@
 export function controller() {
 
-  console.log('Cargado el controller')
+    console.log('Cargado el controller')
 
-  /** Nodos del DOM */
-  let btnMostrar = document.querySelector('#btn-mostrar')
-  let btnSaludar = document.querySelector('#btn-saludar')     
-  let btnLeer = document.querySelector('#btn-leer') 
+    /** Nodos del DOM */
+    let btnMostrar = document.querySelector('#btn-mostrar')
+    let btnSaludar = document.querySelector('#btn-saludar')     
+    let btnLeer = document.querySelector('#btn-leer') 
 
-  /** Manejadores de eventos */
-  btnSaludar.addEventListener('click', saludar)
-  btnMostrar.addEventListener('click', mostrar)
-  btnLeer.addEventListener('click', leerDatos)
-  
+    /** Manejadores de eventos */
+    btnSaludar.addEventListener('click', saludar)
+    btnMostrar.addEventListener('click', mostrar)
+    btnLeer.addEventListener('click', leerDatos)
+    
 
-  function mostrar() {
+    function mostrar() {
 
-      /* setTimeout(saludar,1000)
-      
-      function saludar() {
-          console.log('hola amigo')
-      } */
+        /* setTimeout(saludar,1000)
+        
+        function saludar() {
+            console.log('hola amigo')
+        } */
 
-      setTimeout( ()=>console.log('Item 1'), 0 )
+        setTimeout( ()=>console.log('Item 1'), 0 )
 
-      console.log('Item 2')
+        console.log('Item 2')
 
-      setTimeout( ()=>console.log('Item 3'), 2000 )
-      
-      console.log('Item 4')
+        setTimeout( ()=>console.log('Item 3'), 2000 )
+        
+        console.log('Item 4')
 
-      setTimeout( ()=>console.log('Item 5'), 1000 )        
-      
-  }
+        setTimeout( ()=>console.log('Item 5'), 1000 )        
+        
+    }
 
-  function saludar() {
-      console.log('Hola amigo')
-  }
+    function saludar() {
+        console.log('Hola amigo')
+    }
 
-  function leerDatos() {
+    function leerDatos() {
 
-      let i
-      // Lectura síncrona
-      // i = 33
-      // Lectura síncrona
-      setTimeout(() => {
-          i = 33
-          mostrarDatos()
-      }, 1000);
+        let i
+        // Lectura síncrona: se supone que accedemos a un servidor y eso no existe
+        // i =  '33fdfss747474'
+        // Lectura asíncrona
+        setTimeout(() => {
+            i = '33fdfss747474'
+            mostrarDatos()
+        }, 1000);
 
-      // mostrarDatos() // el resultado sería undefined
+        //mostrarDatos() // el resultado sería undefined
 
-      function mostrarDatos() {
-          console.log(i)
-      }
-      
-  }
+        function mostrarDatos() {
+            console.log(i)
+        }
+        
+    }
 }
--- js_web/11-asinc-promesas/js/controller.js
+++ js_web/11-asinc-promesas/js/controller.js
@@ -4,152 +4,126 @@ export function controller() {
 
     /** Nodos del DOM */
     const btnPedir = document.querySelector('#btn-pedir') 
-    const btnSaludar = document.querySelector('#btn-saludar') 
-    const btnSaludar17= document.querySelector('#btn-saludar-17')
-    const btnUser = document.querySelector('#btn-users') 
-    
-    const output = document.querySelector('#output') 
-   
+    const btnSaludar = document.querySelector('#btn-saludar')
+    const btnSaludar17 = document.querySelector('#btn-saludar-17')
+    const btnUsers = document.querySelector('#btn-users')
+    const output = document.querySelector('#output')
     
     /** Manejadores de eventos */
-
     btnPedir.addEventListener('click', onPedir)
     btnSaludar.addEventListener('click', onSaludar)
-    btnSaludar17.addEventListener('click', onSaludar17)
-    btnUser.addEventListener('click', onUser)
-  
-  
-  let aDatos=[1,2,3,4,5]
-   aDatos .map((x,indice,Array)=>{x*indice})
-   
-   
+    btnUsers.addEventListener('click', onUsers)
+    btnSaludar17.addEventListener('click', onSaludarAwait)
+
+    
     /**
-    * una promesa es un objeto nativo desde ES6
-    * que representa la terminacion o el fracaso eventual
-    * de una operacionasincrona
-    * 
-    * una promesa es un objeto devuelto
-    * al cual encadenas las funciones caballback,
-    * en vex de pasar funciones calback a una fucion
-    * 
-    */
+     * Una "Promesa" es un objeto nativo desde ES6
+     * que representa la terminación o el fracaso eventual 
+     * de una operación asíncrona.
+     * 
+     * Una promesa es un objeto devuelto desde una función
+     * al cual encadenas las funciones callback, 
+     * en vez de pasar funciones callback a una función.
+     * 
+     */
 
-    function completarPedido (){
-        return new Promise((resolve,reject) => {
-        setTimeout(() => {
-         if  (Math.random () > 0.3) { //70%simulamos aciertos 
-            resolve('toma tu hanbrugesa')
-        }
-        else{//el otro  30% simulamos errores
-          reject('lo siento,no me quedan hambrugesas')
-        }
-        },2000)
-    })
+    function completarPedido () {
+        return new Promise( (resolve, reject) => {
+            setTimeout( () => {
+                if (Math.random() > 0.3) { // 70% simulamos aciertos
+                    resolve('Toma tu hamburgesa')
+                } else { // el otro 30% simulamos errores
+                    reject('Lo siento, no quedan hamburgesas')
+                }
+            }, 2000 )
+        })
+    } 
+    
 
-    }
-
-     function onPedir() {
-     
+    function onPedir() {
         completarPedido()
-        .then((datos) => {
-            output.innerHTML +='<li>'+ datos + '</li>'
+        .then( (datos) => { 
+            output.innerHTML += '<li>' + datos + '</li>' 
         })
-        .catch ((error) => {
-            output.innerHTML += '<li>'+ error+ '</li>'
+        .catch( (error) => {
+            output.innerHTML += '<li>' + error + '</li>'
         })
-        
-    }
+    }    
+    
 
-
-
-    /*function msgAfterTimeout (msg, nombre, tiempo, cb) {
+    /* 
+    function msgAfterTimeout (msg, nombre, tiempo, cb) {
         setTimeout(function () {
             cb(msg, nombre);
         }, tiempo);
-    };
+    }; 
     
-
-   
-   
-    
-
-}
- 
- 
-msgAfterTimeout( 'Juanito', 'Pepito', 3500, (msg, nombre) => {
-    let saludo = (`Hola ${nombre}!`)
-    let quienSoy = msg
-    console.log(saludo)
-    msgAfterTimeout(msg, saludo, 500, (quienSoy, saludo) => {
-        console.log(`${saludo}, soy ${quienSoy}`)
-    })
-})        
-*/ 
-
- 
-
-  function msgAfterTimeout(msg,nombre,tiempo){
-      return new Promise((resolve) => {
-          setTimeout(() => {
-              let datos=[msg,nombre]
-              resolve([datos])
-          }, tiempo)
-      })
-  }
-
- 
-function onSaludar(){
-    msgAfterTimeout('Juanito', 'Pepito', 3500)
-     .then( ([datos]) => {
-    
-        let saludo = (`Hola ${datos[1]}!`)
-        let quienSoy = datos[0]
+    msgAfterTimeout( 'Juanito', 'Pepito', 3500, (msg, nombre) => {
+        let saludo = (`Hola ${nombre}!`)
+        let quienSoy = msg
         console.log(saludo)
-        return msgAfterTimeout (quienSoy, saludo, 500)
-     })
-     .then ((datos) => {
-        console.log(`${datos}, soy ${datos}`)
-     })
+        msgAfterTimeout(msg, saludo, 500, (quienSoy, saludo) => {
+            console.log(`${saludo}, soy ${quienSoy}`)
+        }) 
+    })  
+    */
+
+    function msgAfterTimeout(msg, nombre, tiempo) {
+        return new Promise( (resolve) => {
+            setTimeout( ()=>{
+                let datos = [msg, nombre]
+                resolve(datos)
+            }, tiempo)
+        } )
     }
+
+    function onSaludar() {
+        msgAfterTimeout( 'Juanito', 'Pepito', 3500)
+        .then( (datos = []) => {
+            let saludo = (`Hola ${datos[1]}!`)
+            let quienSoy = datos[0].toUpperCase()
+            console.log(saludo)
+            return msgAfterTimeout (quienSoy, saludo, 500)
+        }).then( (datos = []) => {
+            console.log(`${datos[1]}, soy ${datos[0]}`)
+        }) 
+    }
+
+    /* Ejemplo de la misma función usando destructuring de los arrays de datos
+    
+    function onSaludar() {
+        msgAfterTimeout( 'Juanito', 'Pepito', 3500)
+        .then( ([msg, nombre]) => {
+            let saludo = (`Hola ${nombre}!`)
+            let quienSoy = msg
+            console.log(saludo)
+            return msgAfterTimeout (msg, saludo, 500)
+        }).then( ([quienSoy, saludo]) => {
+            console.log(`${saludo}, soy ${quienSoy}`)
+        }) 
+    } */
+
+
     /**
-     * en ES 2017
-     * 
+     * En ES 2017 apraece async / await
      */
-    async function onSaludarAwait(){
-        let datos=[]
-        datos= await msgAfterTimeout('Juanito', 'Pepito', 3500)
+
+    async function onSaludarAwait () {
+        let datos = []
+        datos = await msgAfterTimeout( 'Juanito', 'Pepito', 3500)
         let saludo = (`Hola ${datos[1]}!`)
         let quienSoy = datos[0].toUpperCase()
         console.log(saludo)
         datos = await msgAfterTimeout (quienSoy, saludo, 500)
-            console.log(`${datos[1]}, soy ${datos[0]}`)
-         }
-
-         function onUser(){
-             const url='http://jsonplaceholder.typicode.com/users'
-             fetch(url)
-             .then((response)=>{return response.json()})
-             .then((data)=>{console.log})
-         }
+        console.log(`${datos[1]}, soy ${datos[0]}`)
     }
 
 
-
-
-
-
-
-/** function onSaludar(){
-msgAfterTimeout('Juanito', 'Pepito', 3500)
- .then( ([msg,nombre]) => {
-
-    let saludo = (`Hola ${nombre}!`)
-    let quienSoy = msg
-    console.log(saludo)
-    return msgAfterTimeout (msg, saludo, 500)
- })
- .then ((quienSoy,saludo) => {
-    console.log(`${saludo}, soy ${quienSoy}`)
- })
- }*/ 
-//fetch().then().catch()
+    function onUsers() {
+        const url = 'https://jsonplaceholder.typicode.com/users'
+        fetch(url)
+        .then( (response) => { return response.json()})
+        .then( (data) => { console.dir(data) })
+        
+    }
+}
